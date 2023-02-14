3cb867360ce3f9bf23edc85daf1ffcbdeb682cc0
hulyakucuk <0hulyakucuk@gmail.com>
2023-02-14 23:32:29 +0300

    update script.js

--- sound-stream-deck/script.js
+++ sound-stream-deck/script.js
@@ -4,6 +4,21 @@ sounds.forEach((sound) =>{
     const btn = document.createElement("button")
     btn.classList.add("btn")
 
+    btn.addEventListener("click",() =>{
+        stopSounds();
+        document.getElementById(sound).play()
+    })
+
     btn.innerText=sound
     document.getElementById("buttons").appendChild(btn)
 })
+
+function stopSounds(){
+    sounds.forEach((sound)=>{
+        const song =document.getElementById(sound);
+
+        song.pause();
+        song.currentTime=0;
+    })
+}
+
